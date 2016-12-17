789418bf9904c8497d9beef64f2fc6a575cac710
DrFriendless <friendless.farrell@gmail.com>
2016-12-17 11:16:39 +1100

    UI improvements

--- src/main/resources/js/newgames.js
+++ src/main/resources/js/newgames.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var app = angular.module('newgames', ['ngSanitize', 'ui.select']);
+// https://gist.github.com/ollieglass/f6ddd781eeae1d24e391265432297538
+var kelly_colors = ['#222222', '#F3C300', '#875692', '#F38400', '#A1CAF1', '#BE0032', '#C2B280', '#848482',
+    '#008856', '#E68FAC', '#0067A5', '#F99379', '#604E97', '#F6A600', '#B3446C', '#DCD300', '#882D17', '#8DB600',
+    '#654522', '#E25822', '#2B3D26']
+
 
 function NewGamesCtrl($scope, $http, $timeout, $interval) {
     var vm = this;
@@ -20,7 +25,7 @@ function NewGamesCtrl($scope, $http, $timeout, $interval) {
     vm.dataURL = function() {
         var players = vm.getPlayers();
         if (players.length == 0) return null;
-        return '/json/newgames/2005/' + players.join();
+        return '/json/newgames/2003/' + players.join();
     };
 
     vm.loadData = function() {
@@ -35,6 +40,7 @@ function NewGamesCtrl($scope, $http, $timeout, $interval) {
                 $(function () {
                     var myChart = Highcharts.chart('graph', {
                         chart: { type: 'scatter' },
+                        colors: kelly_colors,
                         title: { text: 'Plays of New Games' },
                         plotOptions: {
                             scatter: {
@@ -48,7 +54,8 @@ function NewGamesCtrl($scope, $http, $timeout, $interval) {
                             }
                         },
                         series: data,
-                        xAxis: { type: 'datetime', title: { text: "Start Time"} }
+                        xAxis: { type: 'datetime', title: { text: "Start Time"} },
+                        yAxis: { title: { text: "Unique Games Played" }}
                     });
                 });
             },
